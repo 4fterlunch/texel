c06a94aebe5b643e524b89aaf21de84510123b18
Michael <mholmes2@student.unimelb.edu.au>
2020-10-26 09:00:45 +1100

    poc push for shader textures demo

--- texel.js
+++ texel.js
@@ -1,22 +1,39 @@
 let $ = require("jquery");
 
-const demoPath = "demo/walnutleaf.png";
+const demoBaseColour = "demo/mossy_bark/Mossy_bark_02_2K_Base_Color.png";
+const demoHeightMap = "demo/mossy_bark/Mossy_bark_02_2K_Height.png";
+const demoNormalMap = "demo/mossy_bark/Mossy_bark_02_2K_Normal.png";
+const demoRoughnessMap = "demo/mossy_bark/Mossy_bark_02_2K_Roughness.png";
 
-function TexelImage(name="") {
+function TexelImage() {
     this.name = name;
     this.filePath = "";
+    this.marvinImage = new MarvinImage();
+    this.set = function(name, filePath) {
+        this.name = name;
+        this.filePath = this.filePath;
+    }
+    this.drawTo = function(canvasElement) {
+        this.marvinImage.load(this.filePath, imageLoaded);
+    }
+    
 }
 
 function TexelProject(name="") {
     this.name = name;
-    this.colourMap = null;
-    this.heightMap = null;
-    this.normalMap = null;
-    this.clearcoatMap = null;
-    this.roughnessMap = null;
-    this.alphaMap = null;
+    this.colourMap = new MarvinImage();
+    this.heightMap = new MarvinImage();
+    this.normalMap = new MarvinImage();
+    this.clearcoatMap = new MarvinImage();
+    this.roughnessMap = new MarvinImage();
+    this.alphaMap = new MarvinImage();
 
     this.settings = {
+        scene: {
+            ambientLight:null,
+            spotlight: null,
+            camera: null,
+        },
         preview: {
             transparency: true,
             opacity: 1.0,
@@ -24,60 +41,116 @@ function TexelProject(name="") {
             clearcoat: 0.0,
             roughness: 0.0,
         }
-
-
-
     }
 }
 
-
-var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x222222 );
-var ambient = new THREE.AmbientLight( 0x222222 ,7);
-scene.add( ambient );
-
-
-var spot1 = new THREE.SpotLight( 0xffffff, 1 );
-spot1.position.set( 300, 300, 500 );
-spot1.angle = 0.80;
-spot1.penumbra = 0.75;
-spot1.intensity = 2;
-spot1.decay = 2;
-
-spot1.castShadow = true;
-spot1.shadow.bias = 0.0001;
-spot1.shadow.mapSize.width = 2048;
-spot1.shadow.mapSize.height = 2048;
-
-scene.add(spot1);
-
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-    var texLoader = new THREE.TextureLoader();
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    var texelContainer = document.getElementById("texel-container");
-    var plane;
-    //ui
+let colourMap = new MarvinImage();
+let heightMap = new MarvinImage();
+let normalMap = new MarvinImage();
+let clearcoatMap = new MarvinImage();
+let roughnessMap = new MarvinImage();
+let alphaMap = new MarvinImage();
 
 
-    texelContainer.appendChild( renderer.domElement );
+var Texel = {
+        scene: new THREE.Scene(),
+        camera: new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 ),
+        renderer : new THREE.WebGLRenderer(),
+        project: null,
+        container:null,
+        plane:null,
+        controls:null,
+        loadProject: function(project=new TexelProject("demo")) {
+            this.scene.background = new THREE.Color( 0x222222 );
+            this.project = project;
+            this.project.settings.scene.ambientLight = new THREE.AmbientLight( 0x222222 ,7);
+            this.project.settings.scene.camera = this.camera;
+            this.scene.add(this.project.settings.scene.ambientLight);
+
+            var spot1 = new THREE.SpotLight( 0xffffff, 1 );
+            spot1.position.set( 300, 1000, 500 );
+            spot1.angle = 0.90;
+            spot1.penumbra = 0.75;
+            spot1.intensity = 1.2;
+            spot1.decay = 4;
+            spot1.castShadow = true;
+            spot1.shadow.bias = 0.0001;
+            spot1.shadow.mapSize.width = 2048;
+            spot1.shadow.mapSize.height = 2048;
+            this.project.settings.scene.spotlight = spot1;
+            this.scene.add(this.project.settings.scene.spotlight);
+
+            
+
+            this.renderer.setSize( window.innerWidth, window.innerHeight );
+            this.renderer.shadowMap.enabled = true;
+            this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+            this.container = document.getElementById("texel-container");
+            
+            this.container.appendChild(this.renderer.domElement);
+
+            this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+            this.camera.position.z = 2000;
+
+        }
+    }
 
 
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    camera.position.z = 2000;
+Texel.loadProject();
+
+//set up 3d textures
+var textureLoader = new THREE.TextureLoader();
+function refreshPlaneTextures() {
+    var colMap, alphaMap, heightMap, ccMap, roughnessMap, normalMap;
+
+    textureLoader.load(demoHeightMap, function(hm) {
+        textureLoader.load(demoNormalMap, function(nm) {
+            textureLoader.load(demoRoughnessMap, function(rm) {
+                textureLoader.load(demoBaseColour, function(bcm) {
+                    var material = new THREE.MeshPhysicalMaterial({
+                        map:bcm, 
+                        side: THREE.DoubleSide,
+                        transparent: true,
+                        bumpMap: hm,
+                        
+                        normalMap: nm,
+                        clearcoatRoughnessMap:rm
+                    });
+                    
+                    var geometry = new THREE.PlaneGeometry(bcm.image.naturalWidth, bcm.image.naturalHeight, 4 );
+                    Texel.plane = new THREE.Mesh( geometry, material );
+                    Texel.scene.add( Texel.plane );
+            }); 
+        });
+    });
+        
+       
+    });
+}
+
+
+
+refreshPlaneTextures();
+    
 
     //img editor
     var image = new MarvinImage();
     var canvas = document.getElementById("editing-canvas");
-    image.load(demoPath, imageLoaded);
+    image.load(demoBaseColour, imageLoaded);
+
+
 
     function imageLoaded() {
         canvas = document.createElement('canvas');
         canvas.id = "editing-canvas";
         var container = document.getElementById("editing-container");
+        console.log(image);
+        container.addEventListener('click', function(ev) {
+            console.log(ev);
+            var ctx = canvas.getContext("2d");
+            console.log(ctx);
+            image.draw(canvas);
+        });
 
         var width = image.getWidth();
         var factor = width / 80;
@@ -86,54 +159,51 @@ scene.add(spot1);
         
         image.draw(canvas);
         container.appendChild(canvas);
+        
+        
+
     }
 
-    texLoader.load(demoPath, function(t) {
-        var material = new THREE.MeshPhysicalMaterial({
-            map:t, 
-            side: THREE.DoubleSide,
-            transparent: true,
-            clearcoat:0.});
-        
-        var geometry = new THREE.PlaneGeometry(t.image.naturalWidth, t.image.naturalHeight, 4 );
-    plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
-    });
+    // var alphaMap = new MarvinImage();
+
+
+    
 
 
 
     var animate = function () {
         requestAnimationFrame( animate );
 
-        if (plane) plane.rotation.y += 0.007;
-        // cube.rotation.y += 0.01;
+        if (Texel.plane)  Texel.plane.rotation.y += 0.007;
         
-        camera.lookAt( scene.position );
-        renderer.render( scene, camera );
+        
+        //Texel.camera.lookAt( Texel.scene.position );
+        Texel.renderer.render( Texel.scene, Texel.camera );
     };
 
-    function loadTexture(path) {
-        console.log(path);
-
-    }
-    $("#file-picker").on("change", function() {
-        loadTexture(document.getElementById("file-picker").files[0].name); 
-        
-        });
 
 
 
-        window.addEventListener( 'resize', onWindowResize, false );
+
+
+
+
+       // window.addEventListener( 'resize', onWindowResize, false );
     animate();
 
-    function onWindowResize() {
+    // function onWindowResize() {
 
-        var windowHalfX = window.innerWidth / 2;
-        var windowHalfY = window.innerHeight / 2;
+    //     var windowHalfX = window.innerWidth / 2;
+    //     var windowHalfY = window.innerHeight / 2;
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+    //     camera.aspect = window.innerWidth / window.innerHeight;
+    //     camera.updateProjectionMatrix();
 
-        // effect.setSize( window.innerWidth, window.innerHeight );
+    //     // effect.setSize( window.innerWidth, window.innerHeight );
 
-    }
+    // }
+
+
+function setColourMap() {
+
+}
